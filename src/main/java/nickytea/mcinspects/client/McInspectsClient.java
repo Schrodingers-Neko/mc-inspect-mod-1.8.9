29468b3c9d39efcc2ab45d4147bf5ea6c8d872ff
NickTriance <114621135+NickTriance@users.noreply.github.com>
2025-01-17 19:21:38 -0800

    Setup inspect button

--- src/main/java/nickytea/mcinspects/client/McInspectsClient.java
+++ src/main/java/nickytea/mcinspects/client/McInspectsClient.java
@@ -1,13 +1,29 @@
 package nickytea.mcinspects.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.util.InputUtil;
 import nickytea.mcinspects.McInspects;
+import net.minecraft.client.option.KeyBinding;
+import org.lwjgl.glfw.GLFW;
+
 
 public class McInspectsClient implements ClientModInitializer {
 
 
+
     @Override
     public void onInitializeClient() {
-        McInspects.LOGGER.info("client go brrr");
+        McInspects.LOGGER.info("MCInspects client initializing...");
+
+        KeyBinding inspectBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("keys.mcinspects.inspectitem", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "keys.category.mcinspects"));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (inspectBinding.wasPressed()) {
+                McInspects.LOGGER.info("User pressed the inspect button");
+            }
+
+        });
     }
 }
