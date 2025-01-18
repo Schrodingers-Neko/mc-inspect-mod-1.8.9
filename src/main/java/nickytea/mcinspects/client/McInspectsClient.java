dddc32813b7d04f6a8785e4c598d5b1b4cebd841
NickTriance <114621135+NickTriance@users.noreply.github.com>
2025-01-17 19:39:47 -0800

    keybinding status
    made a getter for if the inspect button was pressed

--- src/main/java/nickytea/mcinspects/client/McInspectsClient.java
+++ src/main/java/nickytea/mcinspects/client/McInspectsClient.java
@@ -12,13 +12,17 @@ import org.lwjgl.glfw.GLFW;
 
 public class McInspectsClient implements ClientModInitializer {
 
+    static KeyBinding inspectBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("keys.mcinspects.inspectitem", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "keys.category.mcinspects"));
 
+
+    public static boolean isInspecting() {
+        return inspectBinding.isPressed();
+    }
 
     @Override
     public void onInitializeClient() {
         McInspects.LOGGER.info("MCInspects client initializing...");
 
-        KeyBinding inspectBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("keys.mcinspects.inspectitem", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_R, "keys.category.mcinspects"));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (inspectBinding.wasPressed()) {
